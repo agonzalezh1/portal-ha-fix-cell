9f9cab14eee345780f1796adb2c0b8d0120e6d61
adan.gonzalez <adan.gonzalez@elektra.com.mx>
2023-11-02 19:25:00 -0600

    Periodo noviembre

--- src/utils/dbConnect.js
+++ src/utils/dbConnect.js
@@ -14,3 +14,21 @@ export const connectDB = async () => mongoose.connect(dbVariables.DB_URL_CONNECT
 
 export const disconnectDB = async () => mongoose.disconnect();
 
+/**
+ * Query para agregar un nuevo periodo en la base
+ */
+/*
+db.stores.updateOne(
+    { "_id": ObjectId("64d44b7a249ba6229207e2f8") },
+    { $push: { "sales": { "period": "11/2023", "products": { "cashPayment": 0, "cardPayment": 0 }, "fixes": { "cashPayment": 0, "cardPayment": 0 }, "airtime": 0, "spend": 0 } } }
+ )
+ 
+ 
+ 64d44b7a249ba6229207e2f8 -> Bodega
+ 64d44b73249ba6229207e2f4 -> Chairez
+ 64d44b6a249ba6229207e2f0 -> Monte Verde
+ 64d44b63249ba6229207e2ec -> Jesus Maria
+ 64d44b58249ba6229207e2e8 -> Macario
+ 64d44b51249ba6229207e2e4 -> Consti
+ 64d44b42249ba6229207e2df -> Puertecito
+*/
