71845bbce8ce006053d37909864792b2d895cd4d
adan.gonzalez <adan.gonzalez@elektra.com.mx>
2023-11-09 21:24:15 -0600

    Micas 9h en 0 pesos

--- src/components/Sales/ProductsList/ProductsList.js
+++ src/components/Sales/ProductsList/ProductsList.js
@@ -5,6 +5,11 @@ import { useSelector } from 'react-redux';
 import { PRICES_TYPE, USER_TYPE } from '../../../utils/constants';
 import RadioButton from '../../Controllers/RadioButton';
 
+/**
+ * Componente que muestra una lista de productos para que se pueda seleccionar la cantidad y el tipo de precio para su venta
+ * @param {array} productsList Lista de productos devueltos por la busqueda
+ * @param {function} addProduct Funcion que se detona cuando se da clic en Agregar
+ */
 const ProductsList = ({ productsList, addProduct }) => {
 
     const currentStore = useSelector(state => state.stores.currentStore);
@@ -18,11 +23,21 @@ const ProductsList = ({ productsList, addProduct }) => {
     });
     const [priceType, setPriceType] = useState({ type: PRICES_TYPE.PUBLIC, price: productsList[0].publicPrice });
     const [count, setCount] = useState(1);
+    const [showFixRadioButton, setShowFixRadioButton] = useState(false);
 
+    /**
+     * Obtiene el stock del producto de la sucursal
+     * La sucursal es obtenida del almacenamiento, previamente llenado cuando se hace login
+     */
     const getStock = storesList => storesList.find(store => store.id === currentStore).count;
 
     const [currentStock, setCurrentStock] = useState(getStock(productsList[0].stores));
 
+    /**
+     * Valida si se puede incrementar el contador de cantidad con relacion al stock
+     * Si aun hay existencias entonces incrementar el contador
+     * @param {number} value Valor para incrementar o decrementar el contador
+     */
     const validateStock = value => {
         const newValue = count + value;
 
@@ -31,6 +46,10 @@ const ProductsList = ({ productsList, addProduct }) => {
         }
     };
 
+    /**
+     * Establece los valores de los precios, stock y producto seleccionado cuando se selecciona un nuevo valor en la tabla
+     * @param {*} product Producto seleccionado
+     */
     const setProductAndPriceSelected = product => {
 
         const stockTemp = getStock(product.stores);
@@ -38,14 +57,22 @@ const ProductsList = ({ productsList, addProduct }) => {
             return;
         }
 
-        let price = 0;
+        let price;
+
+        if (product.productName.includes('Mica 9h')) {
+            setShowFixRadioButton(true);
+        } else {
+            setShowFixRadioButton(false);
+        }
 
         if (priceType.type === PRICES_TYPE.PUBLIC) {
             price = product.publicPrice;
         } else if (priceType.type === PRICES_TYPE.MID_WHOLE_SALE) {
             price = product.midWholesalePrice;
-        } else {
+        } else if (priceType.type === PRICES_TYPE.WHOLE_SALE){
             price = product.wholesalePrice;
+        } else {
+            price = 0;
         }
         setCurrentStock(stockTemp);
         setProductSelected(product);
@@ -118,6 +145,17 @@ const ProductsList = ({ productsList, addProduct }) => {
                     value={productSelected.wholesalePrice}
                 />
             </>}
+            {showFixRadioButton &&
+                <RadioButton
+                    id={`radio-button-${PRICES_TYPE.FIX}`}
+                    label={`${PRICES_TYPE.FIX} ($0)`}
+                    control={control}
+                    changeEvent={e => setPriceType({ type: e.name, price: Number(e.value) })}
+                    name={PRICES_TYPE.FIX}
+                    state={priceType.type}
+                    value={0}
+                />
+            }
         </div>
         <div className='counter-container'>
             <p className='label bold'>Cantidad </p>
